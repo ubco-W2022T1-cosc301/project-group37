dbbfa82cbc23b39cc273514bdddaa986c0c71500
sahibkhinda <sahibkhinda97@gmail.com>
2022-11-07 23:35:41 -0800

    Push fixes for line plots

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -7,5 +7,7 @@ def load_and_process(fileName):
                        .sort_values("date") 
                        .dropna(axis=0)
                  )
+    loaderDataFrame['reported_year'] = pd.to_datetime(loaderDataFrame['date']).dt.year
+    loaderDataFrame['reported_month'] = pd.to_datetime(loaderDataFrame['date']).dt.month
     
     return loaderDataFrame
